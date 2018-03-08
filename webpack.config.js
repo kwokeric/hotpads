74736076b4bcc0f4044da8707fde8627aac0cb26
kwokeric <eric.kwok@vevo.com>
2018-03-07 19:00:18 -0800

    Add webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,44 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './js/main.js',
+  entry: './js/main.js',  // single js entrypoint
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'main.bundle.js'
+    filename: 'main.[hash].bundle.js'  // output with hashed added to output filename
   },
   module: {
     loaders: [
-      {
-        test: /\.js$/,
+      {        // accepts either js or jsx
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015']
         }
+      },
+      {        // handles .scss extensions with has added to output filename
+        test: /\.scss$/,
+        loader: ['file?name=styles.[hash].css', 'css', 'sass']
+      },
+      {        // handles .jpeg, .jpg, .png, and .gif extensions
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {        // handles .svg extension
+        test: /\.svg$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ],
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
